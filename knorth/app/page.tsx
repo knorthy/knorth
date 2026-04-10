018c00b2e10788a1bb93bf809d7f94239c72f337
Tiffany Lyssa <tiffanylyssa.p@gmail.com>
2026-04-10 17:17:46 +0800

    Include Hero component in the Home page

--- knorth/app/page.tsx
+++ knorth/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero'; // Import the new file here
+import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <main className="pt-24"> {/* pt-24 ensures the hero isn't hidden under the fixed navbar */}
+      <main className="pt-24"> 
         <Hero />
       </main>
     </div>
